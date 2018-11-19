fef01b947aa8cbf2b322e89e2326a56d99e17d7c
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-11-19 19:19:57 +0300

    ZoneCube.gd - optimization function _update_current_type()

--- Prefabs/ZoneCubes/ZoneCube.gd
+++ Prefabs/ZoneCubes/ZoneCube.gd
@@ -93,7 +93,9 @@ func _update_neighbors():
 # внешний вид текущего куба
 func _update_current_node():
 	_update_current_type()
-	current_node = GlobalZoneCube.cubes[current_type].instance()
+	
+	if current_type != GlobalZoneCube.EMPTY:
+		current_node = GlobalZoneCube.cubes[current_type].instance()
 
 
 func _free_current_node():
@@ -106,10 +108,258 @@ func _free_current_node():
 
 ## long if
 func _update_current_type():
-	if (top_cube == null) and (bottom_cube == null) and (right_cube == null) and (left_cube == null) and (front_cube == null) and (back_cube == null):
-		current_type = GlobalZoneCube.SINGLE
+	if top_cube == null:
+		if bottom_cube == null:
+			if right_cube == null:
+				if left_cube == null:
+					if front_cube == null:
+						if back_cube == null:
+							# 000000
+							current_type = GlobalZoneCube.SINGLE
+						else:
+							# 000001
+							pass
+					else:
+						if back_cube == null:
+							# 000010
+							pass
+						else:
+							# 000011
+							pass
+				else:
+					if front_cube == null:
+						if back_cube == null:
+							# 000100
+							pass
+						else:
+							# 000101
+							pass
+					else:
+						if back_cube == null:
+							# 000110
+							pass
+						else:
+							# 000111
+							pass
+			else:
+				if left_cube == null:
+					if front_cube == null:
+						if back_cube == null:
+							# 001000
+							pass
+						else:
+							# 001001
+							pass
+					else:
+						if back_cube == null:
+							# 001010
+							pass
+						else:
+							# 001011
+							pass
+				else:
+					if front_cube == null:
+						if back_cube == null:
+							# 001100
+							pass
+						else:
+							# 001101
+							pass
+					else:
+						if back_cube == null:
+							# 001110
+							pass
+						else:
+							# 001111
+							pass
+		else:
+			if right_cube == null:
+				if left_cube == null:
+					if front_cube == null:
+						if back_cube == null:
+							# 010000
+							pass
+						else:
+							# 010001
+							pass
+					else:
+						if back_cube == null:
+							# 010010
+							pass
+						else:
+							# 010011
+							pass
+				else:
+					if front_cube == null:
+						if back_cube == null:
+							# 010100
+							pass
+						else:
+							# 010101
+							pass
+					else:
+						if back_cube == null:
+							# 010110
+							pass
+						else:
+							# 010111
+							pass
+			else:
+				if left_cube == null:
+					if front_cube == null:
+						if back_cube == null:
+							# 011000
+							pass
+						else:
+							# 011001
+							pass
+					else:
+						if back_cube == null:
+							# 011010
+							pass
+						else:
+							# 011011
+							pass
+				else:
+					if front_cube == null:
+						if back_cube == null:
+							# 011100
+							pass
+						else:
+							# 011101
+							pass
+					else:
+						if back_cube == null:
+							# 011110
+							pass
+						else:
+							# 011111
+							pass
 	else:
-		current_type = GlobalZoneCube.SINGLE
-
-
+		if bottom_cube == null:
+			if right_cube == null:
+				if left_cube == null:
+					if front_cube == null:
+						if back_cube == null:
+							# 100000
+							pass
+						else:
+							# 100001
+							pass
+					else:
+						if back_cube == null:
+							# 100010
+							pass
+						else:
+							# 100011
+							pass
+				else:
+					if front_cube == null:
+						if back_cube == null:
+							# 100100
+							pass
+						else:
+							# 100101
+							pass
+					else:
+						if back_cube == null:
+							# 100110
+							pass
+						else:
+							# 100111
+							pass
+			else:
+				if left_cube == null:
+					if front_cube == null:
+						if back_cube == null:
+							# 101000
+							pass
+						else:
+							# 101001
+							pass
+					else:
+						if back_cube == null:
+							# 101010
+							pass
+						else:
+							# 101011
+							pass
+				else:
+					if front_cube == null:
+						if back_cube == null:
+							# 101100
+							pass
+						else:
+							# 101101
+							pass
+					else:
+						if back_cube == null:
+							# 101110
+							pass
+						else:
+							# 101111
+							pass
+		else:
+			if right_cube == null:
+				if left_cube == null:
+					if front_cube == null:
+						if back_cube == null:
+							# 110000
+							pass
+						else:
+							# 110001
+							pass
+					else:
+						if back_cube == null:
+							# 110010
+							pass
+						else:
+							# 110011
+							pass
+				else:
+					if front_cube == null:
+						if back_cube == null:
+							# 110100
+							pass
+						else:
+							# 110101
+							pass
+					else:
+						if back_cube == null:
+							# 110110
+							pass
+						else:
+							# 110111
+							pass
+			else:
+				if left_cube == null:
+					if front_cube == null:
+						if back_cube == null:
+							# 111000
+							pass
+						else:
+							# 111001
+							pass
+					else:
+						if back_cube == null:
+							# 111010
+							pass
+						else:
+							# 111011
+							pass
+				else:
+					if front_cube == null:
+						if back_cube == null:
+							# 111100
+							pass
+						else:
+							# 111101
+							pass
+					else:
+						if back_cube == null:
+							# 111110
+							pass
+						else:
+							# 111111
+							pass
 
