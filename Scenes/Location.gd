675b7d3ccc309cbb6750be98f0e1caa8d2590d68
Vitaly Lifanoff <vitaly@lifanoff.ru>
2018-11-22 16:05:31 +0300

    оптимизация кода выбора текущей локации

--- Scenes/Location.gd
+++ Scenes/Location.gd
@@ -1,19 +1,16 @@
 extends Spatial
 
+# список локаций игры
+var locations = {
+	GlobalData.GameLocation.LITE: preload("res://Prefabs/Locations/SimpleGround.tscn"),
+	GlobalData.GameLocation.DUNES_01: preload("res://Prefabs/Locations/Dune_01.tscn"),
+}
 
-var lite = preload("res://Prefabs/Locations/SimpleGround.tscn")
-var dunes_01 = preload("res://Prefabs/Locations/Dune_01.tscn")
 
-
+# главный узел Game издает сигнал update_location, который запускает этот метод
 func _on_Game_update_location():
 	for child in get_children():
 		child.queue_free()
 	
-	match GlobalData.current_location:
-		GlobalData.GameLocation.LITE:
-			add_child(lite.instance())
-		GlobalData.GameLocation.DUNES_01:
-			add_child(dunes_01.instance())
-		_:
-			add_child(lite.instance())
+	add_child(locations[GlobalData.current_location].instance())
 
