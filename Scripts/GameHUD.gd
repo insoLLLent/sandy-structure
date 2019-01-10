fc7e6ac15b04aa02f14e87a49bb5dbb9c8bddfea
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-01-10 18:42:01 +0300

    FPS label changed to Speed label

--- Scripts/GameHUD.gd
+++ Scripts/GameHUD.gd
@@ -1,17 +1,13 @@
 extends Control
 
 
-var counter_label_node = null
-var fps_label_node = null
+var counter_score_node = null
+var counter_speed_node = null
 
 func _ready():
-	counter_label_node = $"MarginContainer/VBoxContainer/ScoreContainer/CounterLabel"
-	fps_label_node = $"MarginContainer/VBoxContainer/TestFPS/FPSLabel"
-
-
-func _process(delta):
-	fps_label_node.text = str(Engine.get_frames_per_second())
+	counter_score_node = $"MarginContainer/VBoxContainer/ScoreContainer/CounterLabel"
+	counter_speed_node = $"MarginContainer/VBoxContainer/SpeedContainer/CounterLabel"
 
 
 func _on_Game_update_score_sig(current_score):
-	counter_label_node.text = str(current_score)
+	counter_score_node.text = str(current_score)
