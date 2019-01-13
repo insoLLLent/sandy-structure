18c8f3b6617faaeac827473fe9d37a0b8f5b7346
Vitaly Lifanoff <vitaly@lifanoff.ru>
2019-01-13 12:52:22 +0300

    добавлено отображение счета в конце игры

--- Scenes/DeadEndScreen.gd
+++ Scenes/DeadEndScreen.gd
@@ -1,9 +1,11 @@
 extends Control
 
 var dead_end_animation_node = null
+var counter_score_node = null
 
 func _ready():
 	dead_end_animation_node = $"../../DeadEndAnimation"
+	counter_score_node = $"PanelContainer/CenterContainer/VBoxContainer/HBoxContainer/CounterLabel"
 	hide()
 
 
@@ -15,3 +17,6 @@ func show_dead_end():
 func goto_main_menu():
 	NextSceneLoader.goto_scene(GlobalData.MAIN_MENU_SCENE_RES)
 
+
+func _on_Game_update_score_sig(current_score):
+	counter_score_node.text = str(current_score)
